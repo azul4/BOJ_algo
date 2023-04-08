2ef2a71cb55ec2559985040fb481e075cbac3377
Jaeyoon Kim <azul@kakao.com>
2023-04-08 13:26:33 +0900

    SS 전기버스 백트래킹/메모이제이션 풀이

---
+++ SWExpert/전기버스2 - bt.py
@@ -0,0 +1,37 @@
+import sys; sys.stdin=open("input.txt", "r")
+ans = 999
+change = 0
+changed = False
+c=[]
+def bt(t, fuel, pow):
+    global change, ans, changed
+
+    if changed and change >= ans:
+        return
+    if pow == 0:
+        return
+    if t==fuel[0] and pow >= 0:
+        ans = min(change, ans)
+        changed = True
+        return
+    pow -= 1
+    #change
+    change += 1
+    c.append(t)
+    bt(t+1, fuel, fuel[t])
+    change -= 1
+    c.pop()
+
+    #don't change
+    bt(t+1, fuel, pow)
+
+
+#main
+T = int(input())
+for tc in range(T):
+    fuel = list(map(int, input().split()))
+    bt(2, fuel, fuel[1])
+    print(f"#{tc+1} {ans}")
+    ans = 999
+    change = 0
+    changed = False
